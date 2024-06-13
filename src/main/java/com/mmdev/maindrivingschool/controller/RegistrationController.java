e8d067adea97578a875a1e2896e5cd8e7da14844
viceAdmiralGarp <vyacheslavtihonov636@gmail.com>
2024-06-13 19:16:50 +0300

    lol

--- src/main/java/com/mmdev/maindrivingschool/controller/RegistrationController.java
+++ src/main/java/com/mmdev/maindrivingschool/controller/RegistrationController.java
@@ -2,13 +2,16 @@ package com.mmdev.maindrivingschool.controller;
 
 import com.mmdev.maindrivingschool.entity.RegistrationForm;
 import com.mmdev.maindrivingschool.service.RegistrationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class RegistrationController {
 
 	@Autowired
@@ -16,13 +19,7 @@ public class RegistrationController {
 
 	@PostMapping("/register")
 	@ResponseBody
-	public String register(@RequestParam("fullname") String fullname,
-						   @RequestParam("email") String email,
-						   @RequestParam("phone") String phone) {
-		System.err.println(fullname);
-		System.err.println(email);
-		System.err.println(phone);
-		RegistrationForm form = new RegistrationForm(fullname, email, phone);
+	public String register(@RequestBody RegistrationForm form) {
 		registrationService.register(form);
 		return "Registration Successful!";
 	}
